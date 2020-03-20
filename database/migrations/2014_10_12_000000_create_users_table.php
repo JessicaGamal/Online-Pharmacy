1b9ebc5e3981cf70dc3f93947b99f1cfa62ce1c1
_8andy <58533381+Abanoup-youssef@users.noreply.github.com>
2020-03-20 19:50:08 +0200

    Make a registeration database table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+          $table->string('Firstname');
+          $table->string('Middlename');
+          $table->string('Lastname');
+          $table->string('email')->unique();
+          $table->string('Mobile');
+          $table->string('Phone');
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('password');
+          $table->rememberToken();
         });
     }
 

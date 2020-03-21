0b80f74d0fc7f55e8d452be0cb7c371007a11cc1
sara hussein <sh184610@gmail.com>
2020-03-21 20:18:25 +0200

    handle error messsage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,27 +2,31 @@
 
 @section('content')
 
-<div class="container">
-   <h1>LOGIN</h1>
+         <link href="style.css" rel="stylesheet">
+
+         <div class="c">
+   <h1>login</h1>
  <form method="POST" action="{{ route('login') }}">
             @csrf
 
     <div class="tbox">
-      <label >email address</label>
+    
 
          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="email" required autofocusd>
 
              @error('email')
-               <span class="invalid-feedback" role="alert">
+             
+               <span class="badge badge-primary" role="alert">
+
                 <strong>invalid username or password</strong>
                   </span>
                     @enderror
                     </div>
     <div class="tbox">
-       <label >password</label>
+      
           <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autofocusd>
                 @error('password')
-                    <span class="invalid-feedback" role="alert">
+               <span class="badge badge-primary" role="alert">
                         <strong>invalid username or password</strong>
                              </span>
                                 @enderror
@@ -31,9 +35,11 @@
                                  LOGIN
                 </button>
                 <br>
-    <a class="b1" href="{{ route('register') }}">
+                <div class="b1">
+    <a href="{{ route('register') }}">
                 CREATE ANACCOUNT
         </a>
+      </div>
                     </form>
     </div>
 @endsection

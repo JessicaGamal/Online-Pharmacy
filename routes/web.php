a1fae0a5ebcce077a254712386ab1d9fdb0a2b4d
JessicaGamal <jessicagamal169@gmail.com>
2020-03-24 17:35:56 +0200

    linking between FirstHome and some other pages

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('FirstHome'); });
+Route::get('register', function()
+{
+    return view('register'); });
+
+Route::get('login', function()
+{
+    return view('login'); });
+
+Route::get('FirstHome', function()
+{
+    return view('FirstHome'); });
   Auth::Routes();
   Route::get('/home', 'HomeController@index')->name('home');
     /*

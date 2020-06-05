94bd6a399ef69e2a82437f5a98fdeabc9ca39e8c
JessicaGamal <jessicagamal169@gmail.com>
2020-06-05 15:02:11 +0200

    make seeder and create fixed emails one for admin and another for user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(productsTableSeeder::class);
+         $this->call([productsTableSeeder::class, usersTableSeeder::class]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,7 +68,7 @@
                         <div class="row">
                             <div class="col-lg-6">
                                 <label>Mobile</label>
-                                <input id="Mobile" type="number" class="form-control @error('Mobile') is-invalid @enderror" name="Mobile" value="{{ old('Mobile') }}" required autocomplete="Mobile" placeholder="Enter Your Mobile *11 number at least*">
+                                <input id="Mobile" type="number" class="form-control @error('Mobile') is-invalid @enderror" name="Mobile" value="{{ old('Mobile') }}" required autocomplete="Mobile" placeholder="Enter Your Mobile *11 number*">
 
                                 @error('Mobile')
                                     <span class="badge badge-primary" role="alert">
@@ -80,7 +80,7 @@
 
                             <div class="col-lg-6">
                                 <label>Phone Number</label>
-                               <input id="Phone" type="number" class="form-control @error('Phone') is-invalid @enderror" name="Phone" value="{{ old('Phone') }}"  placeholder="Enter Your Mobile *10 number at least*">
+                               <input id="Phone" type="number" class="form-control @error('Phone') is-invalid @enderror" name="Phone" value="{{ old('Phone') }}"  placeholder="Enter Your Mobile *10 number*">
                                @error('Phone')
                                   <span class="badge badge-primary" role="alert">
                                       <strong>{{ $message }}</strong>
